faaebc5a931e744571369482b0eaeca5e6f9c7b3
Ken <mq.wang@mail.utoronto.ca>
2019-03-13 18:13:14 -0400

    Misc updates; Added some REST APIs

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const EYE_CATCHER = "***********************************************************";
 const fs = require("fs");
 
 const express = require('express');
@@ -110,24 +111,215 @@ app.use(function (req, res, next){
     next();
 });
 
+/* ---- Event APIs ---- */
+const logAPIerror = function(API_route){
+	console.log(EYE_CATCHER);
+	console.log("Error in " + API_route + ":");
+	console.log(error);
+	console.log(EYE_CATCHER);
+};
+
+/*
+ * Create an event and its timetable slots.
+ */
+app.post('/event/create', function(req, res, next){
+	let event_detail = req.body.detail;
+	let timetable_slots = req.body.timetable_slots; // Does not contain an event id
+
+	let num_slots = timetable_slots.length;
+	let num_finished = 0;
+	pool.query("insert into event_detail values (?,?,?,?)", [null].concat(event_detail), function (error, results, fields){
+		if (error) {
+			logAPIerror("/event/create");
+			res.status(500).end(err);
+		}
+		else {
+			// Create the timetable slots
+			for (let i = 0; i < num_slots; i++) {
+				pool.query("insert into timetable_event values(?,?,?,?,?,?,?,?,?,?)", [null, results[0].id].concat(timetable_slots[i]), function (error2, results2, fields2) {
+					if (error2) {
+						logAPIerror("/event/create");
+						res.status(500).end(err);
+					}
+					else {
+						num_finished += 1;
+						if (num_finished == num_slots) {
+							res.json("Event created!");
+						}
+					}
+				});
+			}
+		}
+	});
+	next(); // Correct?
+});
+
+/*
+ * Add a timetable slot for an event.
+ * If any slot is obscured by this slot, update the database accordingly.
+ */
+ app.post('/event/timetable_slot/create', function (req, res, next){
+ 	let timetable_slot = req.body.timetable_slot; // Does contain an event id
+ 	let obscure_id = req.body.obscure; // Slot id
+
+ 	pool.query("insert into timetable_event values(?,?,?,?,?,?,?,?,?,?)", timetable_slot, function (error, results, fields){
+ 		if (error) {
+ 			logAPIerror("/event/timetable_slot/create");
+ 			res.status(500).end(err);
+ 		}
+ 		else {
+ 			// Update the slot that is obscured
+ 			if (obscure_id !== null) {
+ 				pool.query("update timetable_event set obscured_by=? where id=?", [results[0].id, obscure_id], function(error2, results2, fields2){
+ 					if (error2) {
+ 						logAPIerror("/event/timetable_slot/create");
+ 						res.status(500).end(err);
+ 					}
+ 					else {
+ 						res.json("Timetable slot created!");
+ 					}
+ 				});
+ 			}
+ 		}
+ 	});
+ 	next(); // Correct?
+ });
+
+/*
+ * Update an event.
+ */
+app.patch('/event/update', function (req, res, next){
+	let event_detail = req.body.detail;
+	let event_id = req.body.id;
+
+	pool.query("update event_detail set text_content=?, media_content_urls=?, place=? where id=?", event_detail.concat(event_id), function (error, results, fields){
+		if (error) {
+			logAPIerror("/event/update");
+			res.status(500).end(error);
+		}
+		else {
+			res.json("Event updated!");
+		}
+	});
+	next(); // Correct?
+});
+
+/*
+ * Update a timetable slot
+ */
+app.patch('/event/timetable_slot/update', function (req, res, next){
+	let timetable_slot = req.body.timetable_slot;
+	let slot_id = req.body.id;
+
+	pool.query("update timetable_event set start_time=?, length=?, day_of_the_week=? where id=?", timetable_slot.concat(event_id), function (error, results, fields){
+		if (error) {
+			logAPIerror("/event/timetable_slot/update");
+			res.status(500).end(error);
+		}
+		else {
+			res.json("Timetable slot updated!");
+		}
+	});
+	next(); // Correct?
+});
+
+/*
+ * Delete a timetable slot.
+ * If this is actually a slot which obscures another slot, do not delete it but set all fields to null.
+ */
+app.delete('/event/timetable_slot/delete', function (req, res, next){
+	let slot_id = req.body.id;
+
+	pool.query("select id from timetable_event where obscured_by=?", [slot_id], function (error, results, fields){
+		if (error) {
+			logAPIerror("/event/timetable_slot/delete");
+			res.status(500).end(error);
+		}
+		else {
+			if (results[0].length !== 0) {
+				// Set all fields to null
+				pool.query("update timetable_event set is_empty_obscure where id=?", [slot_id], function (error2, results2, fields2){
+					if (error2) {
+						logAPIerror("/event/timetable_slot/delete");
+						res.status(500).end(error2);
+					}
+					else {
+						res.json("Timetable slot deleted!");
+					}
+				});
+			}
+			else {
+				// Delete this slot
+				pool.query("delete from timetable_event where id=?", [slot_id], function (error, results2, fields2){
+					if (error2) {
+						logAPIerror("/event/timetable_slot/delete");
+						res.status(500).end(error2);
+					}
+					else {
+						res.json("Timetable slot deleted!");
+					}
+				});
+			}
+		}
+	});
+	next(); // Correct?
+});
+
+/*
+ * Delete an event and its timetable slots.
+ */
+app.delete("/event/delete", function (req, res, next){
+	let event_id = req.body.id;
+
+	pool.query("delete from event_detail where id=?", [event_id], function (error, results, fields){
+		if (error) {
+			logAPIerror("/event/delete");
+			res.status(500).end(error);
+		}
+		else {
+			// Delete the timetable slots
+			pool.query("delete from timetable_event where event_id=?", [event_id], function (error2 ,results2, fields2){
+				if (error2) {
+					logAPIerror("/event/delete");
+					res.status(500).end(error2);
+				}
+				else {
+					res.json("Event deleted!");
+				}
+			});
+		}
+	});
+	next(); // Correct?
+});
+
+// TODO: Retrieve all events for a given week.
+
+/* ---- Event APIs done ---- */
+
 /*
  * DEBUG
  */
-app.get('/', function (req, res, next){
+app.get('/debug', function (req, res, next){
 	/*pool.query('select ?', ["test query with placeholder"], function (error, results, fields){
 		// results hold the result -- array of RowDataPacket; e.g. results[0].id, results[0].author
 		// fields hold the result set metadata
 		if (error) throw error;
 		res.json(results);
 	});*/
-	pool.query('select * from event', function (error, results, fields){
+	/*pool.query('select * from event', function (error, results, fields){
 		if (error) throw error;
 		res.json(results);
+	});*/
+	let data = [null, "test_event1", "test_user1", "test_content1", "", "some place", false, false];
+	pool.query('insert into event values (?,?,?,?,?,?,?,?)', data, function (error, results, fields){
+		if (error) throw error;
+		res.json(results); // {"fieldCount":0,"affectedRows":1,"insertId":2,"serverStatus":2,"warningCount":0,"message":"","protocol41":true,"changedRows":0}
 	});
 	next();
 });
 
 // TODO: How to insert NULL?
+
 
 /*
  * Response LOG
--- backend/database.sql
+++ backend/database.sql
@@ -1,11 +1,9 @@
-create table event (id int not null auto_increment, 
-	name VARCHAR(50) not null, author VARCHAR(50), 
+create table event_detail (id int not null primary key auto_increment,
 	text_content VARCHAR(300), media_content_urls VARCHAR(300), 
-	place_pc varchar(10) not null references place(postal_code), 
-	primary key(id));
+	place varchar(200) not null references place(name));
 
-create table place (postal_code varchar(10) not null primary key, 
-	name varchar(200) not null, visit_count int not null);
+create table place (name varchar(200) not null primary key, 
+	visit_count int not null);
 
 create table group_event (id int not null primary key references event(id), 
 	invitees varchar(2000), attendees varchar(2000));
@@ -14,7 +12,29 @@ create table comment (id int not null primary key auto_increment,
 	event_id int references event(id), author VARCHAR(50) not null, 
 	content VARCHAR(300) not null, created_at VARCHAR(50) not null);
 
-create table timetable (author VARCHAR(50) not null primary key, 
-	event_id int not null references event(id), year int not null, 
-	month int not null, day int not null, start_time VARCHAR(10), 
-	end_time VARCHAR(10), day_of_the_week int not null);
+create table timetable_event ( id int not null primary key auto_increment,
+	event_id int not null references event(id),
+	author VARCHAR(50) not null, event_name VARCHAR(50) not null,
+	event_has_detail boolean not null, start_time VARCHAR(50) not null, 
+	length float not null, week_of VARCHAR(30) not null,
+	day_of_the_week int not null, is_repeating boolean not null, 
+	obscured_by int not null references timetable_event(event_id),
+	is_empty_obscure boolean not null);
+
+
+
+
+
+
+
+drop table event_detail;
+drop table place;
+drop table group_event;
+drop table comment;
+drop table timetable_event;
+
+describe event_detail;
+describe place;
+describe group_event;
+describe comment;
+describe timetable_event;

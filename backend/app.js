93a3fbb2d471fb12c8a76f98a2a3c1fdc3000ee2
Ken <mq.wang@mail.utoronto.ca>
2019-03-31 13:42:22 -0400

    Fixed timetable slot retrieval API

--- backend/app.js
+++ backend/app.js
@@ -861,8 +861,8 @@ app.get("/event/timetable_slot/retrieveAll/:week_of", isAuthenticated, function 
 	let week_of = validator.escape(req.params.week_of);
 	let author_id = req.session.inAppId;
 
-	pool.query("select * from timetable_event where week_of=? and id not in (select slot_id from obscured_event where week_of=?) \
-		and event_id in (select event_id from event_ownership where author_id=?) or week_of>? and is_repeating=true", [week_of, week_of, week_of, author_id], function (error, results, fields){
+	pool.query("select * from timetable_event where event_id in (select event_id from event_ownership where author_id=?) \
+		and (week_of=? or (week_of<? and is_repeating=true))", [author_id, week_of, week_of], function (error, results, fields){
 		if (error) {
 			logAPIerror("/event/timetable_slot/retrieveAll:week_of", error);
 			res.status(500).end(error);
